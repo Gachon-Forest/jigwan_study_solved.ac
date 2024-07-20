c60dd91881fa0bebcb2a830c728f9ea8dff55655
FutureOfTed <92726782+FutureOfTed@users.noreply.github.com>
2024-07-20 13:19:42 +0900

    Update 2751.py

--- class 2/2751.py
+++ class 2/2751.py
@@ -1,9 +1,8 @@
 # Date: 7/17
-# No: 2609
-# Tier: 브론즈 1
-# Name: 최대공약수와 최소공배수
+# No: 2751
+# Tier: 실버 5
+# Name: 수 정렬하기 2
 # Language: PyPy3
-# 주의 : math 모듈 사용하여 최대공약수를 'gcd'함수를, 최소공배수를 'lcm'함수를 사용
 
 # 1번째 방법
 import sys
